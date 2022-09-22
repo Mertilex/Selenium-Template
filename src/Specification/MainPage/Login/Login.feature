3e3bd4d406189212bffc3acd55a2b3973cc60746
Grzegorz Kowalik <gkowalik@future-processing.com>
2022-09-22 14:04:21 +0200

    Added cross reference between Specification and Atuomation projects. Added implementation of Gherkin scenario.

--- src/Specification/MainPage/Login/Login.feature
+++ src/Specification/MainPage/Login/Login.feature
@@ -7,10 +7,10 @@ So I can use the application when I am logged in
 Background: 
 	Given The User has role all privileges assigned 
 
-@tag1
+#@tag1
 Scenario: User is able to log in to the application using login and password
 	Given Login page is displayed
 	When the User provides login and password
-	And the User clocks on LogIn button
+	And the User clicks on LogIn button
 	Then the User is logged in
 	And Main page of the application is displayed
---
+++ src/Web.Automation/Steps/Login/LoginSteps.cs
@@ -0,0 +1,38 @@
+﻿using TechTalk.SpecFlow;
+
+namespace Web.Automation.Steps.Login
+{
+    [Binding]
+    public class LoginSteps
+    {
+        [Given("Login page is displayed")]
+        public void LoginPageIsDisplayed()
+        {
+            var a = 0;
+        }
+
+        [When("the User provides login and password")]
+        public void TheUserProvidesLoginAndPassword()
+        {
+            var a = 0;
+        }
+
+        [When("the User clicks on LogIn button")]
+        public void TheUserClicksOnLogInButton()
+        {
+            var a = 0;
+        }
+
+        [Then("the User is logged in")]
+        public void TheUserIsLoggedIn()
+        {
+            var a = 0;
+        }
+
+        [Then("Main page of the application is displayed")]
+        public void MainPageOfTheApplicationIsDisplayed()
+        {
+            var a = 0;
+        }
+    }
+}
