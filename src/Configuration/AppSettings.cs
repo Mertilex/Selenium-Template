d55b0bc75680c60d1292633e132a7373546a7b05
Grzegorz Kowalik <gkowalik@future-processing.com>
2022-09-30 13:44:04 +0200

    Added initial version of support for appsettings.json file

---
+++ src/Configuration/AppSettings.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Configuration
+{
+    public static class AppSettings
+    {
+        private static readonly IConfigurationRoot Config;
+
+        public static string Test => Config["Key0"];
+
+        static AppSettings()
+        {
+            Config = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
+                    .Build();
+        }
+    }
+}
